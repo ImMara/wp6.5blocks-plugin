8ecc155f926cccd9fc1325628bdf943c49f2c76e
ImMara <alaneraerts@live.be>
2024-05-28 19:20:35 +0200

    main

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '0f7104eb5c5844f1344c');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '2b24fc60af0fd9a1b455');
